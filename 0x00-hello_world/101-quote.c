c2b1360dfae9d8f33afb8d0b3a66f3129a305c38
zouhirel <zouhairelmallah06@gmail.com>
2023-07-13 19:23:11 -0700

    a

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 */
 int main(void)
 {
-	fput(2,"and that piece of art is useful\" - Dora Korpar, 2015-10
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10
 19\n", stdout);
 	return (1);
 }
