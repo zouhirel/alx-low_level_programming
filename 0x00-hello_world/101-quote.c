a45472ec39f47ef82dc96773e6e7aa0c5adf11bc
zouhirel <zouhairelmallah06@gmail.com>
2023-07-13 18:59:48 -0700

    a

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@
  * Return: Always 0 (Success)
 */
 int main(void)
-{	char quo[] = "and that piece of art is useful" - Dora Korpar, 2015-10-19\n";
+{	char quo[] = "and that piece of art is useful"-Dora Korpar, 2015-10-19\n";
 
 	write(1, quo, 59);
 	return (1);
