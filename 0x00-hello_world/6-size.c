ca52301bd5448478b73e0d1f8ab6f2db7a2f2ca0
zouhirel <zouhairelmallah06@gmail.com>
2023-07-13 17:50:38 -0700

    a

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,8 +8,7 @@
  * Return: Always 0 (Success)
 */
 int main(void)
-{
-        
+{        
 	printf("size of a char: %lu byte(s)\n",sizeof (char));
 	printf("size of an int: %lu byte(s)\n",sizeof (int));
 	printf("size of a long int: %lu byte(s)\n", sizeof(long int));
