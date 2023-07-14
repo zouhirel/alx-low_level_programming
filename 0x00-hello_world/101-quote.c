adba8cff105e4d67e54fcc4929a154f8854ad899
zouhirel <zouhairelmallah06@gmail.com>
2023-07-13 18:52:01 -0700

    a

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,11 +4,12 @@
  * main - Entry point
  *
  * Description: A C program that prints with put function
+ *	ssize_t write(int fd, const void *buf, size_t count);
  *
  * Return: Always 0 (Success)
 */
 int main(void)
-{	char quo[] = "and that piece of art is useful" - Dora Korpar, 2015-10-19\n";
+{	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(1, quo, 59);
 	return (1);
