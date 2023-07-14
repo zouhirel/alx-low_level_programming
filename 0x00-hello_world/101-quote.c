de36f99915da740ad2a33f9d272acbda8b0a4bfa
zouhirel <zouhairelmallah06@gmail.com>
2023-07-13 19:10:23 -0700

    a

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
  *
- * Description: A C program that prints with put function
- *	ssize_t write(int fd, const void *buf, size_t count);
- *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
 */
 int main(void)
-{	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(1, quo, 59);
+{
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10
+19\n", 59);
 	return (1);
 }
