bbc29e6cc0ab197f2c1be4c965c35920757d93cd
zouhirel <zouhairelmallah06@gmail.com>
2023-07-18 08:35:59 -0700

    a

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "_putchar.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
+return (0);
+}
