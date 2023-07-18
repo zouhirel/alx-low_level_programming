8c292a124898c00aaac67bf7158628f18f93ed09
zouhirel <zouhairelmallah06@gmail.com>
2023-07-18 08:50:29 -0700

    «a»

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "_putchar.h"
+#include "main.h"
 /**
  * main - Entry point
  *
@@ -12,6 +12,7 @@ _putchar('p');
 _putchar('u');
 _putchar('t');
 _putchar('c');
+_putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
